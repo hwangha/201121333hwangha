d6c962c2c374d1fab67bca6ebbb0c85bb0b3140f
hwangha <ghkdgk488@naver.com>
2017-06-19 03:29:12 +0900

    sample

---
+++ t6.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+	void *p;
+
+	write(1, "BEFORE MALLOC #0\n", 17);
+	p = malloc(1024);
+	write(1, "AFTER MALLOC #0\n", 16);
+	printf("%p\n", p);
+
+	 write(1, "BEFORE MALLOC #1\n", 17);
+     p = malloc(1024);
+     write(1, "AFTER MALLOC #1\n", 16);
+     printf("%p\n", p);
+
+     write(1, "BEFORE MALLOC #2\n", 17);
+     p = malloc(1024);
+     write(1, "AFTER MALLOC #2\n", 16);
+     printf("%p\n", p);
+
+     write(1, "BEFORE MALLOC #3\n", 17);
+     p = malloc(1024);
+     write(1, "AFTER MALLOC #3\n", 16);
+     printf("%p\n", p);
+
+	 getchar();
+
+	 return(EXIT_SUCCESS);
+}
